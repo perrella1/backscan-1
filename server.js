fc6f20eb8619af58adf99bf0aaf9297f33a0fc99
Pedro Perrella <pedro.perrella@icloud.com>
2025-04-25 19:11:31 -0300

    Ajustes

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
-
-app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/receber-localizacao", async (req, res) => {
+app.post("/api/receber-localizacao", async (req, res) => {
   const { latitude, longitude } = req.body;
 
   if (!latitude || !longitude) {
-    return res.status(400).json({ error: "Latitude ou longitude ausente." });
+    return res.status(400).json({ success: false, message: "Faltam dados de localização." });
   }
-
-  console.log("Localização recebida:");
-  console.log("Latitude:", latitude);
-  console.log("Longitude:", longitude);
 
   try {
     await axios.post("https://hooks.zapier.com/hooks/catch/21524692/2puxt6w/", {
       latitude,
-      longitude,
+      longitude
     });
 
-    res.status(200).json({ success: true });
+    return res.status(200).json({ success: true });
   } catch (error) {
-    console.error("Erro ao enviar para o Zapier:", error.message);
-    res.status(500).json({ success: false, message: "Erro ao enviar para Zapier." });
+    console.error("Erro ao enviar pro Zapier:", error.message);
+    return res.status(500).json({ success: false, message: "Erro ao enviar para o Zapier." });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+module.exports = serverless(app);
